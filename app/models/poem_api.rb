975565c3aad8a5068ddf1fcd6b8d431118bac192
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-21 12:58:24 -0800

    fixed space query

--- app/models/poem_api.rb
+++ app/models/poem_api.rb
@@ -1,13 +1,15 @@
 class PoemApi
   def fetch_poems(query)
-    author_url = "https://poetrydb.org/author/#{query}"
+    q = query.split(" ").join("%20")
+    author_url = "https://poetrydb.org/author/#{q}"
     author_uri = URI.parse(author_url)
     author_response = (JSON.parse(Net::HTTP.get_response(author_uri).body))[0..10]
 
-    title_url = "https://poetrydb.org/title/#{query}"
+
+    title_url = "https://poetrydb.org/title/#{q}"
     title_uri = URI.parse(title_url)
     title_response = (JSON.parse(Net::HTTP.get_response(title_uri).body))[0..20]
-    
+
     if author_response && title_response
       results = (author_response + title_response)
     elsif author_response && title_response==nil
@@ -18,8 +20,6 @@ class PoemApi
       results = nil
     end
     results
-
-   
 
   end
 end
