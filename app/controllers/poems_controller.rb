b3e45442f69132735c245847d839c1dbe7d91ac7
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-22 15:51:39 -0800

    parser fixes

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -2,6 +2,6 @@ class PoemsController < ApplicationController
   def index
 
     @poems = PoemApi.new.scrape_poems(params[:q])
-  
+    
   end
 end
--- app/models/poem_api.rb
+++ app/models/poem_api.rb
@@ -20,22 +20,13 @@ class PoemApi
     #   results = []
     # end
     # results
-    file = File.read('pfd.json')
-    results = JSON.parse(file)
-    
-    by_title = results.select{|result| result["title"].downcase.include?(query.downcase)}
-    by_author = results.select{|result| result["author"].downcase.include?(query.downcase)}
-    by_text = results.select{|result| result["text"].include?(query)}
-    by_keyword = results.select{|result| result["keywords"].include?(query)}
-    poems = (by_author + by_title + by_keyword + by_text).uniq
-    poems
   end
 
   def scrape_poems(query)
     search_url = "https://www.poetryfoundation.org/search?query=#{query}&refinement=poems"
     html = URI.open(search_url)
     doc = Nokogiri::HTML(html)
-    links = (doc.css('.c-feature-hd').css('h2').css('a').collect{|link| link.attribute('href').value})[0..11]
+    links = (doc.css('.c-feature-hd').css('h2').css('a').collect{|link| link.attribute('href').value})[0..40]
     base =  "https://www.poetryfoundation.org"
     poem_data = []
     links.each do |link|
@@ -44,12 +35,15 @@ class PoemApi
       doc = Nokogiri::HTML(html).css('.c-feature')
       title = doc.css('.c-feature-hd').css('h1').text.split.join(" ")
       author = doc.css('.c-feature-sub').css('span').css('a').text
-      lines = doc.css('.o-poem').css('div').collect{|div| div.text.split.join(" ")}
+      lines = doc.css('.o-poem').to_html.gsub("<a", "--")
+      # byebug
       if title != "" && author != ""
         poem_data << {title: title, author: author, lines: lines}
       end
     end
 
     poem_data
+    
+    
   end
 end
