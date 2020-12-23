258a42fe9fdcb8410babcf0f9c407d685bee90e2
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-23 11:47:19 -0800

    fetcher updates

--- app/models/poem_api.rb
+++ app/models/poem_api.rb
@@ -45,11 +45,22 @@ class PoemApi
   # end
 
   def scrape_poems(query)
+    base =  "https://www.poetryfoundation.org"
     search_url = "https://www.poetryfoundation.org/search?query=#{query}&refinement=poems"
     html = URI.open(search_url)
     doc = Nokogiri::HTML(html)
-    # titles = doc.css('.c-feature-hd').collect{|hd| hd.text.split(" ")}.collect{|title|title.join(" ")}
-    links = (doc.css('.c-feature-hd').css('h2').css('a').collect{|link| link.attribute('href').value})[0..40]
-    # byebug
+    headings = (doc.css('.c-feature'))[0..40]
+    poems = []
+    headings.each do |heading|
+      title = heading.css('.c-feature-hd').text.split(" ").join(" ")
+      author = heading.css('.c-feature-sub').css('span').children.text
+      url = (heading.css('h2').css('a').collect{|link| link.attribute('href').value})[0]
+      if url != nil
+        link = base + url
+        poems << {title: title, author: author, link: link}
+      end   
+    end
+    poems
+
   end
 end
